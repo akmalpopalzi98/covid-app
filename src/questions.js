938a225fa95757260699c8da0077efbb0a133102
mudasir100 <99201930+mudasir100@users.noreply.github.com>
2023-12-16 11:38:18 -0800

    Update questions.js

--- src/questions.js
+++ src/questions.js
@@ -1,14 +1,14 @@
 const data = [
   {
-    question: "What is 1+1?",
-    answer: 2,
-    options: [1, 5, 7, 2],
+    question: "How does COVID-19 primarily spread among people?",
+    answer: "C. Close person-to-person contact" ,
+    options: ["A. Through contaminated food", "B. By mosquito bites","C. Close person-to-person contact", "D. Through sunlight exposure"],
     detail: "extra detail1",
   },
   {
-    question: "What is 1+6?",
-    answer: 7,
-    options: [1, 7, 8, 6],
+    question: "What does the “19” in “COVID-19” refer to?",
+    answer: "D. The coronavirus and the disease it causes were identified in 2019",
+    options: ["A. There are 19 variants of the coronavirus", "B. There are 19 symptoms of coronavirus disease", "C. This is the 19th coronavirus pandemic", "D. The coronavirus and the disease it causes were identified in 2019"],
     detail: "extra detail2",
   },
 ];
