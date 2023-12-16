61ba1fd6e4923bdb0d9727cd31346c18667997f5
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-12-16 13:46:05 +0000

    feat: added react-particles background

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,33 @@ import "./app.css";
 import { GiHealthCapsule } from "react-icons/gi";
 import data from "./questions";
 import QuestionCard from "./components/QuestionCard";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect, useMemo } from "react";
 import { ScoreContext } from "./context/ScoreContext";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 
 function App() {
   const { score, isAnswered } = useContext(ScoreContext);
   const [index, setIndex] = useState(0);
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
 
   const nextQuestion = () => {
     // Check if the current question has been answered
@@ -26,9 +47,97 @@ function App() {
     }
   };
 
+  const options = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#0d47a1",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+          resize: true,
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: false,
+          speed: 6,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+            area: 800,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
+  if (init) {
+  }
+
   return (
-    <div style={{ backgroundColor: "#e59866", height: "100vh" }}>
-      <h1 style={{ padding: "10px", backgroundColor: "#e5b966" }}>
+    <div style={{ height: "100vh" }}>
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+      <h1
+        style={{
+          padding: "10px",
+          backgroundColor: "#e5b966",
+          position: "relative",
+          zIndex: 2,
+        }}
+      >
         Your Health <GiHealthCapsule />
       </h1>
       <div
@@ -44,7 +153,7 @@ function App() {
           style={{
             width: "80%",
             height: "80%",
-            backgroundColor: "#e5b966",
+            backgroundColor: "rgba(131, 145, 146,0.6)",
             display: "flex",
             flexDirection: "column",
             justifyContent: "center",
