e47ee99cbf2f1dbc09f73eee4b08fe22a01a52f9
mudasir100 <99201930+mudasir100@users.noreply.github.com>
2023-12-16 12:02:43 -0800

    Update questions.js

--- src/questions.js
+++ src/questions.js
@@ -1,16 +1,29 @@
 const data = [
   {
-    question: "How does COVID-19 primarily spread among people?",
+    question: "1. How does COVID-19 primarily spread among people?",
     answer: "C. Close person-to-person contact" ,
     options: ["A. Through contaminated food", "B. By mosquito bites","C. Close person-to-person contact", "D. Through sunlight exposure"],
     detail: "extra detail1",
   },
   {
-    question: "What does the “19” in “COVID-19” refer to?",
+    question: "2. What does the “19” in “COVID-19” refer to?",
     answer: "D. The coronavirus and the disease it causes were identified in 2019",
     options: ["A. There are 19 variants of the coronavirus", "B. There are 19 symptoms of coronavirus disease", "C. This is the 19th coronavirus pandemic", "D. The coronavirus and the disease it causes were identified in 2019"],
-    detail: "extra detail2",
+    detail: "The correct answer is D. The coronavirus and the disease it causes were identified in 2019. A new coronavirus, technically named SARS-CoV-2, and the disease it causes were first identified in 2019"
   },
+  {
+    question: "3. What are common symptoms of COVID-19?",
+    answer: "B. Fever, cough, and tiredness",
+    options: ["A. Itchy skin", "B. Fever, cough, and tiredness", "C. Hair loss", "D. Increased hunger"],
+    detail: "The correct answer is D. The coronavirus and the disease it causes were identified in 2019. A new coronavirus, technically named SARS-CoV-2, and the disease it causes were first identified in 2019"
+  },
+  {
+  question: "3. What are common symptoms of COVID-19?",
+    answer: "B. Fever, cough, and tiredness",
+    options: ["A. Itchy skin", "B. Fever, cough, and tiredness", "C. Hair loss", "D. Increased hunger"],
+    detail: "The correct answer is D. The coronavirus and the disease it causes were identified in 2019. A new coronavirus, technically named SARS-CoV-2, and the disease it causes were first identified in 2019"
+  },
+
 ];
 
 export default data;
