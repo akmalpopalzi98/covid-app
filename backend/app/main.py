6d7e2072df33dc70333e75089c81241136b58ee3
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-12-21 13:58:52 +0000

    chore: enabling CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from .routers import auth, user
 from . import models
 from .database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 
 models.Base.metadata.create_all(engine)
@@ -12,4 +13,12 @@ app = FastAPI()
 app.include_router(auth.router)
 app.include_router(user.router)
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to the origin(s) of your React app
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
